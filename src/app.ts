1fe0ee2641f6c9e2d72495aaf763d30e584bbf9b
AsadFahimTEC <fah485434@gmail.com>
2025-12-28 21:06:09 +0600

    authentication error fix and implement email verification

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import express, { type Application } from "express";
+import express, { Application } from "express";
 
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import { postRouter } from "./modules/post/post.routes";
 import { getRouter } from "./modules/get/get.router";
+import cors from 'cors';
 
 const app: Application = express();
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true
+}))
 
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use("/posts", postRouter);
 // app.use("/posts", getRouter);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,11 +7,12 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    trustedOrigins: [process.env.APP_URL!],
     user: {
         additionalFields: {
             role: {
                 type: "string",
-                defaultValue:"USER",
+                defaultValue: "USER",
                 required: false
             },
             phone: {
@@ -27,5 +28,12 @@ export const auth = betterAuth({
     },
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
     },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            console.log("******** Verification email send!");
+        },
+    }
 });
