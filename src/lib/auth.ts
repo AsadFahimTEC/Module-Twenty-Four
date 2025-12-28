5ec0f03aa5debfbf8c3d82a4b7b9854400d62617
AsadFahimTEC <fah485434@gmail.com>
2025-12-28 21:47:22 +0600

    email verification token & flow design

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    auth: {
+        user: process.env.APP_USER,
+        pass: process.env.APP_PASS,
+    },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -33,7 +43,16 @@ export const auth = betterAuth({
     },
     emailVerification: {
         sendVerificationEmail: async ({ user, url, token }, request) => {
-            console.log("******** Verification email send!");
+            const verificationUrl = `${process.env.APP_URL}/verify-email/?token=${token}`
+            const info = await transporter.sendMail({
+                from: '"Prisma Blog" <prismablog@ph.com>',
+                to: "fah485434@gmail.com",
+                subject: "Hello ✔",
+                text: "Hello world?", // Plain-text version of the message
+                html: "<b>Hello world?</b>", // HTML version of the message
+            });
+
+            console.log("Message sent:", info.messageId);
         },
     }
 });
